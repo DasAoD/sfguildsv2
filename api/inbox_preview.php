f5469033a0070de77507f9856bb25cae08538b1f
DasAoD <42589682+DasAoD@users.noreply.github.com>
2026-02-24 20:53:01 +0100

    Update inbox_preview.php

--- api/inbox_preview.php
+++ api/inbox_preview.php
@@ -6,6 +6,7 @@
 
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/sf_helpers.php';
 
 header('Content-Type: application/json');
 
